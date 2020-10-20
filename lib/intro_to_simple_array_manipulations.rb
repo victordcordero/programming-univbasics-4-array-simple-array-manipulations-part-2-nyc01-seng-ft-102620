4ed68b3e4e4932175769ee9499a0f88cc802c75d
Victor Cordero <victordcordero@gmail.com>
2020-10-20 16:03:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,8 @@ def using_concat(array_one,array_two)
   array_one.concat(array_two)
 end
 
-def using_insert(array,element)
-  array.insert(-2)
+def using_insert(array)
+  array.insert(-2, )
 end
 
 def using_uniq(array)
