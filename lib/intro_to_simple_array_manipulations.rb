8dc0f9d5789947190e759344ce93e35340a4ba2e
Victor Cordero <victordcordero@gmail.com>
2020-10-20 16:19:31 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+def using_concat(array_one,array_two)
+  array_one.concat(array_two)
+end
+
+def using_insert(array,element)
+  array.insert(4,"Python")
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
